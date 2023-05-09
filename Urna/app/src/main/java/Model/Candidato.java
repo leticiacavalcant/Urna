ec2cfb50b458aebfaaacf4200cc78cbcd58c62fd
leticiacavalcant <leticiacavalcant@academico.ufs.br>
2023-05-09 18:21:54 -0300

    Refazendo projeto com JAVA 11

---
+++ Urna/app/src/main/java/Model/Candidato.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author letic
+ */
+public class Candidato extends Participantes {
+    private int id;
+    private String nome;
+    private int votos = 0;
+
+    public Candidato(int id, String nome) {
+        super(id, nome);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    @Override
+    public void adicionarVoto() {
+       votos++;
+    }
+    
+}
---
+++ Urna/app/src/main/java/Model/Participantes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author letic
+ */
+public abstract class Participantes {
+    private int id;
+    private String nome;
+
+    public Participantes(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public abstract void adicionarVoto();
+    // implementar metodo adicionarVoto() para participantes
+    // registrar um único voto para o usuário e os votos dos candidatos
+    
+    
+    
+}
